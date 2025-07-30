ab219f31403fd5a521363b409f7f95e8ba470648
siubax <siubax2@gmail.com>
2025-07-30 03:43:50 +0100

    Preparing for ad-free build

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,67 +1,21 @@
-import { BannerAd, BannerAdSize } from '@react-native-google-mobile-ads/admob';
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { View } from 'react-native';
-
 import { TabBarIcon } from '@/components/navigation/TabBarIcon';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { Tabs } from 'expo-router';
+import React from 'react';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
-
-  const adUnitID = "ca-app-pub-9767437471456053/3480532627";
-
   return (
-    <View style={{ flex: 1 }}>
-      <Tabs
-        screenOptions={{
-          tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-          headerShown: false,
-        }}>
-        <Tabs.Screen
-          name="index"
-          options={{
-            title: 'Home',
-            tabBarIcon: ({ color, focused }) => (
-              <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-            ),
-          }}
-        />
-        <Tabs.Screen
-          name="search"
-          options={{
-            title: 'Search',
-            tabBarIcon: ({ color, focused }) => (
-              <TabBarIcon name={focused ? 'search' : 'search-outline'} color={color} />
-            ),
-          }}
-        />
-        <Tabs.Screen
-          name="favorites"
-          options={{
-            title: 'Favorites',
-            tabBarIcon: ({ color, focused }) => (
-              <TabBarIcon name={focused ? 'heart' : 'heart-outline'} color={color} />
-            ),
-          }}
-        />
-        <Tabs.Screen
-          name="explore"
-          options={{
-            title: 'Categories',
-            tabBarIcon: ({ color, focused }) => (
-              <TabBarIcon name={focused ? 'grid' : 'grid-outline'} color={color} />
-            ),
-          }}
-        />
-      </Tabs>
-      
-      <BannerAd
-        unitId={adUnitID}
-        size={BannerAdSize.BANNER}
-        onAdFailedToLoad={(error) => console.error(error)}
-      />
-    </View>
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        headerShown: false,
+      }}>
+      <Tabs.Screen name="index" options={{ title: 'Home', tabBarIcon: ({ color, focused }) => (<TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />), }} />
+      <Tabs.Screen name="search" options={{ title: 'Search', tabBarIcon: ({ color, focused }) => (<TabBarIcon name={focused ? 'search' : 'search-outline'} color={color} />), }} />
+      <Tabs.Screen name="favorites" options={{ title: 'Favorites', tabBarIcon: ({ color, focused }) => (<TabBarIcon name={focused ? 'heart' : 'heart-outline'} color={color} />), }} />
+      <Tabs.Screen name="explore" options={{ title: 'Categories', tabBarIcon: ({ color, focused }) => (<TabBarIcon name={focused ? 'grid' : 'grid-outline'} color={color} />), }} />
+    </Tabs>
   );
 }
